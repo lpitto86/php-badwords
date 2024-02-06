943bde07ea4b29793d04fa1518b6ca0c4ec938b0
lpitto86 <luca.p481@gmail.com>
2024-02-06 18:51:43 +0100

    index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <div>
+        <form action="./about.php" method="POST">
+
+            <textarea name="paragraph" cols="30" rows="10"></textarea>
+            <input type="text" name="badword" value="">
+
+            <button type="submit">
+                Invia
+            </button>
+
+        </form>
+    </div>
+</body>
+</html>
